cc5f53be676dcb8015bb3576b48d59355ddd1aa9
Jawahar S <jawahar273@gmail.com>
2022-03-17 22:32:04 +0530

    undo data type

--- core/request.go
+++ core/request.go
@@ -7,22 +7,22 @@ import (
 )
 
 type DefaultQuery struct {
-	PageNumber int32
-	PerPage    int32
+	PageNumber int64
+	PerPage    int64
 }
 
 func DefaultQueryString(c *gin.Context) DefaultQuery {
 
-	pageNumber, _ := strconv.ParseInt(c.DefaultQuery("page", "1"), 10, 32)
-	perPage, _ := strconv.ParseInt(c.DefaultQuery("perPage", "20"), 10, 32)
+	pageNumber, _ := strconv.ParseInt(c.DefaultQuery("page", "1"), 10, 64)
+	perPage, _ := strconv.ParseInt(c.DefaultQuery("perPage", "20"), 10, 64)
 	// limiting to max of 50 at a time
 	if perPage > 50 {
 		perPage = 50
 	}
 
 	return DefaultQuery{
-		PageNumber: int32(pageNumber),
-		PerPage:    int32(perPage),
+		PageNumber: pageNumber,
+		PerPage:    perPage,
 	}
 
 }
